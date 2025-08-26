e3140a42f69deb66c266ab335802cb700971e9be
anurag-si <anuragis4063@gmail.com>
2025-08-27 00:49:15 +0530

    Added README for project

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const liveMatchRoutes = require('./routes/liveMatch');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middlewar
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
